c9c70196220d973a8aa29c9e03f37f61c4d1adf0
xuejundu <xuejundusnow@gmail.com>
2023-08-23 16:03:05 -0400

    add error handler

--- backend/controllers/employeeControllers.js
+++ backend/controllers/employeeControllers.js
@@ -1,26 +1,32 @@
+const asyncHandler = require("express-async-handler");
+
 // @description get all employees
 // @route GET /employee
-const getEmployee = (req, res) => {
+const getEmployee = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get employee" });
-};
+});
 
 // @description create an employee
 // @route POST /employee
-const createEmployee = (req, res) => {
+const createEmployee = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("error");
+  }
   res.status(200).json({ message: "create employee" });
-};
+});
 
 // @description update an employee entry
 // @route PUT /employee/:id
-const updateEmployee = (req, res) => {
+const updateEmployee = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "update employee" });
-};
+});
 
 // @description delete an employee
 // @route DELETE /employee/:id
-const deleteEmployee = (req, res) => {
+const deleteEmployee = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "delete employee" });
-};
+});
 
 module.exports = {
   createEmployee,
